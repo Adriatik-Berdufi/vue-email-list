7413a8073563dacde74661b90b48e357c1d7b3fb
Adriatik Berdufi <adratik.berdufi@gmail.com>
2024-02-06 19:41:27 +0100

    add random 10 email

--- main.js
+++ main.js
@@ -3,15 +3,24 @@ const app = createApp({
     data(){
         return{
             title: 'Mail generator',
+            mailList:[],
         };
     },
     methods: {
-       
+     
+        getEmailAPI(i) {             
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((mail) => {
+                    const risultato = mail.data.response;
+                    this.mailList.push(risultato);
+               });
+                                  
+        },
+        getNeEmails() {for(let i = 0; i < 10; i++) {this.getEmailAPI(i);}},
     },
-    
+       
     mounted(){
         console.log('App montata');
-        
     },
 });
 app.mount('#app');
